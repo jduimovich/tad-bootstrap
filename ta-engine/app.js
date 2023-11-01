03098594a49d19e06e5516b81c365b890fb99f41
John Duimovich <jduimovich@gmail.com>
2023-11-01 10:23:27 -0400

    wip - nunjunks

--- ta-engine/app.js
+++ ta-engine/app.js
@@ -1,21 +1,59 @@
 nunjucks = require('nunjucks')
-nunjucks.configure('pipelines', { autoescape: true, throwOnUndefined: true });
- 
-var pacvars = [ "revision", "git_auth_secret", "repo_url" ] 
-var tabvars = { 
+nunjucks.configure('pipelines', { autoescape: true
+	// , throwOnUndefined: true 
+});
+
+// profile configurations
+// templates are maintained in Backstage "values.valueName format"
+// you can use any "template" format you like inside of {{ value }} format
+
+// Expand templates profiles.
+// gitops does a full expansion default "tad" replacements
+// pac does a full expansion default "tad" replacements leaves the pac custom vars
+// backstage is a direct copy into backstage templates
+
+var profile= "tad"
+
+// PaC templates use these so they get protected in any expansion
+var pac_protected_vars = [
+	"event_type",
+	"git_auth_secret",
+	"pull_request_number",
+	"repo_name",
+	"repo_owner",
+	"repo_url",
+	"revision",
+	"sender",
+	"source_branch",
+	"source_url",
+	"target_branch",
+	"target_namespace"
+]
+
+var tad_vars = { 
+
+}
+var tab_vars = { 
+	"parameters": { 
+		"appname": "${{ parameters.appname }}"
+	} ,
 	"TAB_APP": "${{ parameters.appname }}",
 	"TAB_COMPONENT": "YAY-component-replacement"
 }
-var pac-expand  = { 
+var pacexpand  = { 
 	"TAB_APP": "${{ parameters.appname }}",
 	"TAB_COMPONENT": "YAY-component-replacement"
 }
 // keep PaC variables as same, do not expand.
-pacvars.forEach(element => {
+pac_protected_vars.forEach(element => {
 	tabvars[element] = "{{" + element + "}}"
 });
-console.log (JSON.stringify(tabvars, null, 4))
-nunjucks.render('test.yaml', tabvars,
+
+// run the expansion 
+var templates_vars = {}
+var filename = "'test.yaml'"
+console.log (JSON.stringify(templates_vars, null, 4))
+nunjucks.render(filename, templates_vars,
 	function (err, res) {
 		if (err) { 
 			console.log(err)
