c3ab09202af989150335f959d1138d1470d19050
John Duimovich <jduimovich@gmail.com>
2023-11-21 12:59:11 -0500

    update install

---
+++ installer/helm/install.sh
@@ -0,0 +1,2 @@
+helm repo add rhtap-dance https://redhat-appstudio.github.io/helm-repository
+helm install rhtap-dance/dance --generate-name
---
+++ installer/subscriptions/README.md
@@ -0,0 +1,4 @@
+
+### Install Pipelines and Gitops via Operators
+
+`oc apply -f installer/subcriptions    `
